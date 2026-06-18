1fcf990aa821fe9e05f07b8cb2cd0a6fcc0f4d8a
Carlos Eduardo <carloseduardomveloso@gmail.com>
2026-06-18 15:18:53 -0300

    O básico de data foi feito, mas precisa de alguns complementos

---
+++ src/data/navigation.js
@@ -0,0 +1,7 @@
+export const navigation = {
+    github: 'https://github.com/Carlosmveloso',
+    linkedin: 'https://www.linkedin.com/in/carlos-eduardo-a5294a19b/',
+    whatsapp: 'teste',
+    email: 'carloseduardomveloso@gmail.com',
+    instagram: 'https://www.instagram.com/carlosmveloso_/',
+}
---
+++ src/data/timeline.js
@@ -0,0 +1,23 @@
+export const timeline = [
+  {
+    year: 2022,
+    title: "Início da minha jornada",
+    description: "Comecei meu curso de Análise e Desenvolvimento de sistemas",
+  },
+  {
+    year: 2024,
+    title: "Aprofundamento em JavaScript",
+    description:
+      "Depois da faculdade, encontrei uma linguagem ao qual me identifiquei.",
+  },
+  {
+    year: 2025,
+    title: "Meu primeiros projetos",
+    description: "Meu primeiro projeto Farol Pitimbu",
+  },
+  {
+    year: 2026,
+    title: "Me aprofundando nos conhecimentos",
+    description: "Fiz senac e outras coisas",
+  },
+];
