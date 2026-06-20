ab62bc9e875093cc98041e04b62c699bd500eed6
Carlos Eduardo <carloseduardomveloso@gmail.com>
2026-06-20 19:31:31 -0300

    colocando o @theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header"
 import { features } from "./data/features"
 
 function App() {
   const texto = 'text-blue-400'
   return (
     <>
-      <h1 className={`${texto}`}>Teste</h1>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+function Header() {
+    return (
+        <>
+            <section className="bg-gradient-hero">
+                <section>
+                    <h1 className="font-head">TEste</h1>
+                    <h1 className="font-head">TEste</h1>
+                    <h1 className="font-head">TEste</h1>
+                    <h1 className="font-head">TEste</h1>
+                    <h1 className="font-head">TEste</h1>
+                    <h1 className="font-head">TEste</h1>
+                    <h1 className="font-head">TEste</h1>
+                    <h1 className="font-head">TEste</h1>
+                    <h1 className="font-head">TEste</h1>
+
+                </section>
+                <section>
+                </section>
+            </section>
+        </>
+    )
+};
+
+export default Header;
